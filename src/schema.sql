f289d24de8360f70a31d640d6ad2ae5e8856daa7
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2022-12-01 16:25:35 +0200

    lisää tietokanta viitteille

--- src/schema.sql
+++ src/schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS users (
     username TEXT,
     password TEXT
 );
+
+CREATE TABLE IF NOT EXISTS citations (
+    id INTEGER PRIMARY KEY,
+    authors TEXT,
+    title TEXT,
+    year INTEGER
+);
