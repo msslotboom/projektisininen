6358f79799dacf0d6c6588c6ce24317c81aa5fc9
Sampo <57003465+realtalin@users.noreply.github.com>
2022-12-02 09:40:07 +0200

    Korjaa fly.io tietokantaosoite

--- src/cfg.py
+++ src/cfg.py
@@ -7,3 +7,7 @@ load_dotenv(dotenv_path=os.path.join(directory, "..", ".env"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+# Fly.io fix
+DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
