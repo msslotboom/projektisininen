2deba375d2e5dc1d774da5fba83bfe1473823826
Sampo <57003465+realtalin@users.noreply.github.com>
2022-12-06 02:33:57 +0200

    Korjaa lataustesti

--- src/services/bibgen.py
+++ src/services/bibgen.py
@@ -1,12 +1,16 @@
 from services.citation_services import CitationService
+from os import path
 
 class Bibgen:
     def __init__(self, _citations=CitationService()):
         self._citations = _citations
+
         
 
     def generate_bib_file(self, owner_id):
-        with open ('src/templates/download.html', 'w') as gen:
+        this_directory = path.dirname(__file__)
+        download_html_path = path.join(this_directory, "..", "templates", "download.html")
+        with open (download_html_path, 'w') as gen:
             for citation in self._citations.get_citations(owner_id):
                 gen.write(f' author:  {citation[0]}\n title:  {citation[1]}\n year:  {citation[2]}\n\n')
                 
