d76656ffd8eb49f267a43b712f548abfd43f71eb
pomiska <miska99@live.fi>
2022-12-09 03:39:48 +0200

    lisätty yksikkötestejä citation_servicelle

--- src/tests/unit/citation_services_test.py
+++ src/tests/unit/citation_services_test.py
@@ -7,3 +7,36 @@ class TestCitationService(unittest.TestCase):
         self.citation_repository_mock = Mock()
         self.citation_service = CitationService(self.citation_repository_mock)
 
+    def test_validate_citation_with_no_author(self):
+        try:
+            self.citation_service.create_citation(1, "", "title", "1", "1")
+        except:
+            UserInputError("Viitteestä puuttuu tietoja")
+
+        self.citation_repository_mock.create_new_citation.assert_not_called()
+
+    def test_validate_citation_with_no_title(self):
+        try:
+            self.citation_service.create_citation(1, "author", "", "1", "1")
+        except:
+            UserInputError("Viitteestä puuttuu tietoja")
+
+        self.citation_repository_mock.create_new_citation.assert_not_called()
+
+    def test_validate_citation_with_no_year(self):
+        try:
+            self.citation_service.create_citation(1, "author", "title", "", "1")
+        except:
+            UserInputError("Viite on virheellinen")
+
+        self.citation_repository_mock.create_new_citation.assert_not_called()
+
+    #Jostain syystä citation_servicen check_duplicate_given_id ei toimi oikein testeissä
+    #
+    #def test_create_citation_with_valid_information(self):
+    #    self.citation_service.create_citation(1, "author", "title", 1, 1)
+    #
+    #    self.citation_repository_mock.create_new_citation.assert_called()
+
+    
+
