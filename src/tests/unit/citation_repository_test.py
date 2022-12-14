f778087f7456acf1c02a8e9fd4df04f13a8b2ac3
pomiska <miska99@live.fi>
2022-12-14 22:02:54 +0200

    citation repo unittests

--- src/tests/unit/citation_repository_test.py
+++ src/tests/unit/citation_repository_test.py
@@ -2,47 +2,62 @@ import unittest
 
 from models.book import Book
 from models.user import User
+from models.othercitation import OtherCitation
+from models.article import Article
 from repositories.citation_repository import citation_repository
 from repositories.user_repository import user_repository
 
 
 class TestCitationRepository(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
+
+    def setUp(self):
         citation_repository.delete_all_citations()
         user_repository.delete_all_users()
         user_repository.create_new_user(
             User(username="Jaakko", password="salasana1"))
         self.omistaja_id = 1
 
+    def test_create_new_book_citation(self):
+        citation_repository.create_new_book_citation(
+            Book(owner_id=self.omistaja_id, given_id="Stephen_Hawking_A_Brief_History_Of_Time", author="Stephen Hawking", editor="Unknown", title="A Brief History of Time", publisher="Unknown", year=1988))
+        self.assertEqual(
+            len(citation_repository.get_all_citations(self.omistaja_id)), 1)
+
+    def test_create_new_article_citation(self):
+        citation_repository.create_new_article_citation(Article(
+            owner_id=self.omistaja_id, given_id="Author_Title", author="Author", title="Title", journal="Journal", year=1111))
+        self.assertEqual(
+            len(citation_repository.get_all_citations(self.omistaja_id)), 1)
+
+    def test_create_new_other_citation(self):
+        citation_repository.create_new_other_citation(OtherCitation(
+            owner_id=self.omistaja_id, given_id="Author_Title", author="Author", title="Title", type="Type", other="Other", year=1111))
+        self.assertEqual(
+            len(citation_repository.get_all_citations(self.omistaja_id)), 1)
+
     def test_delete_book_citation(self):
         poistettava = citation_repository.create_new_book_citation(
-            Book(owner_id=self.omistaja_id, given_id=1, author="Stephen Hawking", title="A Brief History of Time", year=1988))
+            Book(owner_id=self.omistaja_id, given_id="Stephen_Hawking_A_Brief_History_Of_Time", author="Stephen Hawking", editor="Unknown", title="A Brief History of Time", publisher="Unknown", year=1988))
+        citation_repository.delete_citation(
+            poistettava.given_id, self.omistaja_id)
 
-        citation_repository.delete_citation(poistettava.id, "book")
-
-        citations = citation_repository.get_all_citations(
-            self.omistaja_id).count()
-        self.assertEqual(citations, 0)
+        self.assertEqual(
+            len(citation_repository.get_all_citations(self.omistaja_id)), 0)
 
     def test_delete_book_citation_a_second_time(self):
         poistettava = citation_repository.create_new_book_citation(
-            Book(owner_id=self.omistaja_id, given_id=1, author="Stephen Hawking", title="A Brief History of Time", year=1988))
-
-        citation_repository.delete_citation(poistettava.id, "book")
-
-        citations = citation_repository.get_all_citations(
-            self.omistaja_id).count()
-        self.assertEqual(citations, 0)
+            Book(owner_id=self.omistaja_id, given_id="Stephen_Hawking_A_Brief_History_Of_Time", author="Stephen Hawking", editor="Unknown", title="A Brief History of Time", publisher="Unknown", year=1988))
+        citation_repository.delete_citation(
+            poistettava.given_id, self.omistaja_id)
+        self.assertEqual(
+            len(citation_repository.get_all_citations(self.omistaja_id)), 0)
 
     def test_delete_one_book_citation_of_two(self):
         poistettava = citation_repository.create_new_book_citation(
-            Book(owner_id=self.omistaja_id, given_id=1, author="Stephen Hawking", title="A Brief History of Time", year=1988))
+            Book(owner_id=self.omistaja_id, given_id="Stephen_Hawking_A_Brief_History_Of_Time", author="Stephen Hawking", editor="Unknown", title="A Brief History of Time", publisher="Unknown", year=1988))
         citation_repository.create_new_book_citation(
-            Book(owner_id=self.omistaja_id, given_id=2, author="Richard Feynman", title="The Feynman Lectures on Physics", year=1963))
-
-        citation_repository.delete_citation(poistettava.id, "book")
-
-        citations = citation_repository.get_all_citations(
-            self.omistaja_id).count()
-        self.assertEqual(citations, 1)
+            Book(owner_id=self.omistaja_id, given_id="Author_Two_Still_Unknown", author="Author Two", editor="Still Unknown", title="Title Two", publisher="Dunno", year=1111))
+        citation_repository.delete_citation(
+            poistettava.given_id, self.omistaja_id)
+        self.assertEqual(
+            len(citation_repository.get_all_citations(self.omistaja_id)), 1)
